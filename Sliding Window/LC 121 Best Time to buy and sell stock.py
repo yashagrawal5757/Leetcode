219b8f3d82e7aeee5dd83dfa105f7e6ee85b1906
Yash Agrawal <yashagrawal5757@gmail.com>
2024-10-30 11:37:11 -0700

    Updated LC 121

--- Sliding Window/LC 121 Best Time to buy and sell stock.py
+++ Sliding Window/LC 121 Best Time to buy and sell stock.py
@@ -1,18 +1,30 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        i,j = 0,1
-        profit = 0
-        while(j<len(prices)):
-            if (prices[i]> prices[j]):
-                i+= 1
-                if (i==j):
-                    j += 1
-            else:
-                #answer
-                profit = max(prices[j] - prices[i], profit)
-                j+= 1
-        return profit
-        
+        #Postfix technique : TC = o(n), SC = O(n)
+        '''
+        postfix = [0] * len(prices)
+        for i in range(len(prices)-2, -1,-1):
+            postfix[i] = max(postfix[i+1],prices[i+1])
+        differences = []
+        max_difference = 0
+        for i in range(0,len(postfix)):
+            difference = postfix[i] - prices[i]
+            max_difference = max(max_difference, difference)
 
+        return max_difference     
+        '''
+        #Sliding window : TC = O(n), SC = O(1)
+        i,j = 0,1
+        maxprofit = 0
+        while(j<len(prices)):
+            profit = prices[j] - prices[i]
+            if profit<=0:
+                i = j
+                j+= 1
+            else:
+                maxprofit = max(maxprofit,profit)
+                j+= 1
+                #if a no lower than ith comes it will automatically go to if & move i to j
+        return maxprofit
 
         
